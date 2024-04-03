c6cfee119edae546abf696cf7f8e1ac958d00a50
Peter Müller <peter@baggers.io>
2024-04-03 15:42:07 +0200

    feat: add test component sk-sketch

--- apps/demo-app/src/app/app.component.ts
+++ apps/demo-app/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SketchComponent } from '@qupaya/sketch';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, SketchComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
