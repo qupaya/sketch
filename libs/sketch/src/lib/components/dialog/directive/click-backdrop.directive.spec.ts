f7a25b45140e035a5b24b55db120bc5f2186be26
inga <inga@qupaya.com>
2024-05-08 14:36:23 +0200

    refactor: remove unit test

--- libs/sketch/src/lib/components/dialog/directive/click-backdrop.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ClickBackdropDirective } from './click-backdrop.directive';
-
-describe('DialogDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ClickBackdropDirective();
-    expect(directive).toBeTruthy();
-  });
-});
